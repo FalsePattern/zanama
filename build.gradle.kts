1fb34755638c20e0af8d7036195f9d6c6b46a78d
FalsePattern <me@falsepattern.com>
2025-10-17 02:12:56 +0200

    ci: 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 group = "com.falsepattern"
 
-version = "0.1.0"
+version = "0.2.0"
 
 java {
     toolchain {
--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 group = "com.falsepattern"
 
-version = "0.1.0"
+version = "0.2.0"
 
 kotlin {
     jvmToolchain(21)
